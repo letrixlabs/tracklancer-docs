1df9edfdff13d88da73d2de145bb464839401845
Harrison Ratcliffe <harrison.ratcliffe@me.com>
2023-10-27 19:09:40 +0100

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -10,8 +10,31 @@ const config: DocsThemeConfig = {
     link: 'https://discord.com',
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  banner: {
+    dismissible: false,
+    key: 'banner-1',
+    text: <a href="https://tracklancer.io" target="_blank">
+      These docs are not fully published/finalised yet.
+    </a>,
+  },
   footer: {
-    text: 'Nextra Docs Template',
+    text: (
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}>
+          <p style={{
+            fontSize: '14px',
+          }}>
+            Copyright &copy; {new Date().getFullYear()} <a href="https://tracklancer.io" target="_blank">Tracklancer</a>. All rights reserved.
+          </p>
+          <p style={{
+            fontSize: '12px',
+          }}>
+            Tracklancer is a Letrix Labs product.
+          </p>
+        </div>
+    ),
   },
 }
 
